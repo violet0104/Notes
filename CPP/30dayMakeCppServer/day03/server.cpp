30ee60c4fca6e2a6d0446b9be18f0647b165e5c5
zdc <3170877204@qq.com>
2026-01-26 21:10:50 +0800

    feat: finished day05-Channel

--- CPP/30dayMakeCppServer/day03/server.cpp
+++ CPP/30dayMakeCppServer/day03/server.cpp
@@ -56,7 +56,7 @@ int main() {
                 socklen_t clnt_addr_len = sizeof(clnt_addr);
                 bzero(&clnt_addr, sizeof(clnt_addr));
 
-                int clnt_sockfd = accept(sockfd, (sockaddr*)&clnt_addr, &clnt_addr_len);
+                int clnt_sockfd = accept(sockfd, (sockaddr *)&clnt_addr, &clnt_addr_len);
                 errif(clnt_sockfd == -1, "socket accept error");
 
                 printf("new client fd %d! IP: %s Port: %d\n", clnt_sockfd, inet_ntoa(clnt_addr.sin_addr), ntohs(clnt_addr.sin_port));
